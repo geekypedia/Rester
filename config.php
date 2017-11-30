235d8d9837efa48dd62006aeffe685b78275c423
steelerp <32609461+steelerp@users.noreply.github.com>
2017-12-01 03:15:44 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+//The api version, must have a php file on versions folder to include
+define('API_VERSION', "1.0.0");
+
+//Database credentials
+define('DBHOST', 'localhost');
+define('DBNAME', 'mydb');
+define('DBUSER', 'dbuser');
+define('DBPASSWORD', 'dbpassword');
+
+//If enabled, verbose log written on error.log
+//define('LOG_VERBOSE', true);
+
+//The path where the uploads are saved. Must be writtable by the webserver
+define('FILE_UPLOAD_PATH', 'uploads');
+
+//Enables API Cache. For now only APC is implemented
+define('CACHE_ENABLED', true);
+
+//Enable OAuth 1.0 Authentication
+define('ENABLE_OAUTH', false);
+
+//Disable PHP Errors
+error_reporting(0);
+
+?>
