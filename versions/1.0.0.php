09e24f8467f8de929a4f003c9bcdf6dfaf0c7819
steelerp <32609461+steelerp@users.noreply.github.com>
2017-12-04 06:32:37 +0530

    Create 1.0.0.php

---
+++ versions/1.0.0.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+* Sample custom login command
+*/
+//Create the command
+$loginCommand = new RouteCommand("POST", "users", "login", function($params = NULL) {
+	
+	global $resterController;
+	
+	$filter["login"]=$params["login"];
+	$filter["password"]=md5($params["password"]);
+	
+	$result = $resterController->getObjectsFromRouteName("usuarios", $filter);
+
+	$resterController->showResult($result);
+	
+}, array("login", "password"), "Method to login users");
+
+//Add the command to controller
+$resterController->addRouteCommand($loginCommand);
+
+//Disable oauth authentication for certain routes
+$resterController->addPublicMethod("POST", "users/login");
+
+//Add file processor. parameter db_name, db_field. will update the db field based on relative path
+$resterController->addFileProcessor("files", "file");
+
+//Custom API
+$helloWorldApi = new RouteCommand("GET", "hello", "world", function($params=null){
+	$api = new ResterController();
+	$value = $api->query("select 'world' as 'hello'"); //you can do any type of MySQL queries here.
+	$api->showResult($value);
+}, array(), "Hello World Api");
+$resterController->addRouteCommand($helloWorldApi);
+
+?>
