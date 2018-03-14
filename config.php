86341261bd242b1fc65504db18333396f0f8d847
Om Talsania <omtalsania@geekypedia.net>
2018-03-15 05:14:32 +0530

    Define MAX_NESTING_LEVEL in config

--- config.php
+++ config.php
@@ -27,6 +27,7 @@ define('DEFAULT_LOGIN_API', false);
 
 //Enable deep nested queries
 define('ENABLE_DEEP_QUERY', true);
+define('MAX_NESTING_LEVEL', 5);
 
 //Disable PHP Errors
 error_reporting(0);
