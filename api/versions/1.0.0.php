945630c4fe17aee6bc60adb96d5ff28af1789b6f
Om Talsania <omtalsania@geekypedia.net>
2018-09-18 17:34:32 +0000

    Webhook error handling

--- api/versions/1.0.0.php
+++ api/versions/1.0.0.php
@@ -311,8 +311,12 @@ $approveFunction = function($params = NULL) {
 		
 		$organization = $api->getObjectsFromRouteName("organizations", $filter);
 		
-		if(function_exists('on_organization_activated')){
-			on_organization_activated($organization, $result);
+		try{
+			if(function_exists('on_organization_activated')){
+				on_organization_activated($organization, $result);
+			}
+		} catch (Exception $ex){
+			
 		}
 
 		$api->showResult($result);
