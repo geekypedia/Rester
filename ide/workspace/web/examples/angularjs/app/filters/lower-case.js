f1997bcfaffe49d0149ea2b355f74b7ba8426d8f
Om Talsania <omtalsania@geekypedia.net>
2018-09-22 20:27:41 +0000

    AngularJS Complex Example

---
+++ ide/workspace/web/examples/angularjs/app/filters/lower-case.js
@@ -0,0 +1,6 @@
+app.filter('lowerCase', function() {
+    return function(input) {
+      input = input || '';
+      return input.replace(/\w\S*/g, function(txt){return txt.toLowerCase();});
+    };
+});  
---
+++ ide/workspace/web/examples/angularjs/app/filters/upper-case.js
@@ -0,0 +1,6 @@
+app.filter('upperCase', function() {
+    return function(input) {
+      input = input || '';
+      return input.replace(/\w\S*/g, function(txt){return txt.toUpperCase();});
+    };
+});
