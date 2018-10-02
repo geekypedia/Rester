4f2c665b6f9a2ca7ef712ed623d3b1f167a5d594
Om Talsania <omtalsania@geekypedia.net>
2018-10-02 15:40:26 +0000

    Default token

--- ide/workspace/web/examples/angularjs/seed.sql
+++ ide/workspace/web/examples/angularjs/seed.sql
@@ -33,7 +33,7 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `token` varchar(50) NOT NULL,
+  `token` varchar(50) NOT NULL DEFAULT '00000000-00000-0000-0000-000000000000',
   `lease` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `role` varchar(50) DEFAULT 'user',
   `is_active` tinyint(1) NOT NULL DEFAULT '1',  
