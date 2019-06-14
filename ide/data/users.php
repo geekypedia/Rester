4fa2dcd04b85ad5bfc5446d0e2bce789e2b2f98f
Om Talsania <omtalsania@geekypedia.net>
2019-06-14 22:46:49 +0530

    Multi user accounts

--- ide/data/users.php
+++ ide/data/users.php
@@ -1,3 +1,3 @@
 <?php
-/*|[{"username":"admin","password":"90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad","project":"web"}]|*/
+/*|[{"username":"superadmin","password":"55057a011158053a051aaabd870e79447d477055","project":"api"},{"username":"admin","password":"90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad","project":"web"},{"username":"user","password":"60bddb16409a2baf76936619afecf778dabe68de","project":"web"}]|*/
 ?>
