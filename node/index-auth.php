7153362bd3d1c70770706a3d657ad636c623def8
Om Talsania <omtalsania@geekypedia.net>
2019-06-15 00:04:09 +0530

    Multi-user support

--- node/index-auth.php
+++ node/index-auth.php
@@ -9,48 +9,39 @@ define("REL_PATH_THIS", ".."); //prod
 function authenticate($relative_path = "./"){
 
 	$users_file = $relative_path . REL_PATH_THIS . '/ide/data/users.php';
-	$reference_obj = null;
 
+    function read_passwords($users_file){
+        $auth_pwds = array();
+    	$data = file_get_contents($users_file);
+    	$startpos = strpos($data, "[");
+    	$endpos = strrpos($data, "]");
+    	$len = $endpos - $startpos + 1;
+    	$realdata = substr($data, $startpos, $len);
+    	$obj = (array) json_decode($realdata, true);
+    	for($i = 0; $i < count($obj) ; $i++){
+    	    $user = $obj[$i]['username'];   
+    	    $password = $obj[$i]['password'];
+    	    $auth_pwds[$user] = $password;
+    	}
+    	return $auth_pwds;	
+    }
+    $reference_obj = read_passwords($users_file);
 
-	function fill_reference_obj($users_file){
-	  global $reference_obj;
-		$data = file_get_contents($users_file);
-		$startpos = strpos($data, "[");
-		$endpos = strrpos($data, "]");
-		$len = $endpos - $startpos + 1;
-		$realdata = substr($data, $startpos, $len);
-		$obj = (array) json_decode($realdata, true);
-		$reference_obj = $obj[0];
-	}
+	$ref_users = array_keys($reference_obj);
 	
-	fill_reference_obj($users_file);
-	
-	function get_password(){
-	  global $reference_obj;
-		return $reference_obj['password'];	
-	}
-	
-	function get_username(){
-	  global $reference_obj;
-		return $reference_obj['username'];	
-	}
-	
-	
-	$PASSWORD = get_password();
-	$USERNAME = get_username();
 	$SUPPLIED_PASSWORD = !empty($_POST["password"]) ? $_POST["password"] : $_REQUEST["password"];
 	$SUPPLIED_PASSWORD_ENC = sha1(md5($SUPPLIED_PASSWORD));
 	$SUPPLIED_USERNAME = !empty($_POST["username"]) ? $_POST["username"] : $_REQUEST["username"];
 	
-	if($SUPPLIED_PASSWORD_ENC == $PASSWORD && $SUPPLIED_USERNAME == $USERNAME){
+	if(in_array($SUPPLIED_USERNAME, $ref_users) && $SUPPLIED_PASSWORD_ENC == $reference_obj[$SUPPLIED_USERNAME]){
 		return array("username" => $SUPPLIED_USERNAME, "password" => $SUPPLIED_PASSWORD);
 	} else {
 		return false;
 	  //header('Location: .?auth=false');
 	  //exit();
-	}	
+	}
+	
 }
-
 
 
 ?>
