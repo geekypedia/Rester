cf5f902e7eb30e13e7525fa5786a997afed80949
Om Talsania <omtalsania@geekypedia.net>
2019-07-05 02:05:35 +0530

    Support for Lua and Luvit

--- lua/secure/index.php
+++ lua/secure/index.php
@@ -190,6 +190,15 @@ if(!$auth){
 				var err = e.responseJSON;
 				if(cb) cb(err);
 			});
+			
+			if(command == 'start') {
+				setTimeout(function() {
+					act('logs', function(r){
+						actionHandler(r);
+					})					
+				}, 5000);
+			}
+			
 		}
 		
 		function refreshStatus(){
