8cdc5c7c251f0255d789f6e19ead21d8b5cf0a5e
Om <om@MacBook-Pro.local>
2019-11-30 05:43:36 +0530

    Remove old material files

--- ide/workspace/web/examples/angularjs-material/app/filters/to-date-time.js
+++
@@ -1,6 +0,0 @@
-/*global app, Helper*/
-app.filter('toDateTime', function() {
-    return function(str) {
-        return Helper.toDateTime(str);
-    };
-});
--- ide/workspace/web/examples/angularjs-material/app/modules/auth/out-of-service.js
+++
@@ -1,5 +0,0 @@
-/*global app*/
-app.controller('outOfServiceController', function($scope, H){
-	$scope.H = H;
-	$scope.M = H.M;
-});
