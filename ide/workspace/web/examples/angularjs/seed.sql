b4b1b0e2872e9e368faa16789ad3cebc6c326d1e
Om Talsania <omtalsania@geekypedia.net>
2019-12-04 03:02:20 +0530

    Update seed.sql

--- ide/workspace/web/examples/angularjs/seed.sql
+++ ide/workspace/web/examples/angularjs/seed.sql
@@ -17,15 +17,18 @@ CREATE TABLE `organizations` (
   `email` varchar(100) NOT NULL,
   `license` varchar(15) NOT NULL DEFAULT 'basic',
   `validity` datetime NOT NULL,  
-  `org_secret` varchar(50) NOT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `theme_bg` varchar(20) DEFAULT NULL,
+  `theme_col` varchar(20) DEFAULT NULL,  
   `is_active` tinyint(1) NOT NULL DEFAULT '0',  
+  `org_secret` varchar(50) NOT NULL,
   `secret` varchar(50) NOT NULL DEFAULT '206b2dbe-ecc9-490b-b81b-83767288bc5e',
   PRIMARY KEY (`id`),
   UNIQUE KEY `org_secret` (`org_secret`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `organizations` (`id`, `name`, `email`, `license`, `validity`, `is_active`, `org_secret`, `secret`) VALUES
-(1,	'Default Organization',	'superadmin@example.com', 'super',	'0000-00-00 00:00:00', 1, '206b2dbe-ecc9-490b-b81b-83767288bc5e',	'206b2dbe-ecc9-490b-b81b-83767288bc5e');
+(1,	'Default Organization',	'superadmin@example.com', 'super',	'0000-01-01 00:00:00', 1, '206b2dbe-ecc9-490b-b81b-83767288bc5e',	'206b2dbe-ecc9-490b-b81b-83767288bc5e');
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
@@ -34,7 +37,7 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(100) NOT NULL,
   `token` varchar(50) NOT NULL DEFAULT '00000000-00000-0000-0000-000000000000',
-  `lease` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lease` datetime NOT NULL DEFAULT '0000-01-01 00:00:00',
   `role` varchar(50) DEFAULT 'user',
   `is_active` tinyint(1) NOT NULL DEFAULT '1',  
   `secret` varchar(50) NOT NULL DEFAULT '206b2dbe-ecc9-490b-b81b-83767288bc5e',
@@ -50,6 +53,22 @@ INSERT INTO `users` (`id`, `email`, `username`, `password`, `token`, `lease`, `r
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `token`, `lease`, `role`, `is_active`, `secret`) VALUES
 (3,	'user@example.com',	'user',	'ee11cbb19052e40b07aac0ca060c23ee',	'1',	'0000-00-00 00:00:00',	'user', 1, '206b2dbe-ecc9-490b-b81b-83767288bc5e');
+
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE `profiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `theme_bg` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `theme_col` varchar(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `secret` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 
 DROP TABLE IF EXISTS `groups`;
 CREATE TABLE `groups` (
