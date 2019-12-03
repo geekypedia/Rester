b26a73e00573937626ad9681b088243f7af7e99e
Om Talsania <omtalsania@geekypedia.net>
2019-12-03 16:17:36 +0530

    Update controller.js

--- ide/workspace/web/examples/angularjs/app/modules/settings/controller.js
+++ ide/workspace/web/examples/angularjs/app/modules/settings/controller.js
@@ -1,7 +1,9 @@
 /*global app*/
-app.controller('settingsController', function($scope, $rootScope, $http, $cookies, H, M){
+app.controller('settingsController', function($scope, $rootScope, $http, $cookies, Alert, H, M, S){
+	
 	$scope.H = H;
 	$scope.M = H.M;
+	
 	
 	$scope.locked = true;
 	
@@ -16,7 +18,32 @@ app.controller('settingsController', function($scope, $rootScope, $http, $cookie
 	$scope.forms = {};
 	
 	$scope.data = {
-	    single: $rootScope.currentUser.organization
+	    single: JSON.parse(JSON.stringify($rootScope.currentUser.organization))
 	};
+	
+	$scope.reloadData = function(){
+		$scope.data.single = JSON.parse(JSON.stringify($rootScope.currentUser.organization));
+	}
+	
+	$scope.save = function(){
+		if($rootScope.currentUser.organization.id){
+			$http.put(S.baseUrl + '/organizations', $rootScope.currentUser.organization).then(function(r){
+				Alert.success("Successfully updated the record!")	
+				$rootScope.currentUser.organization = r.data;
+				$scope.lock();
+			}, function(e){
+				Alert.danger("Error updating the record!")	
+				$scope.reloadData();
+				$scope.lock();
+			});
+			
+		} else {
+			Alert.warning("You are not authorized for updating this information!");
+			$scope.reloadData();
+			$scope.lock();
+		}
+
+	}
+	
 
 });
