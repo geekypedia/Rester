9a23956406547c80ce48942c8ffde877acea3fee
Om Talsania <omtalsania@geekypedia.net>
2019-12-04 18:55:21 +0530

    Update controller-registration.js

--- ide/workspace/web/examples/angularjs/app/init/controller-registration.js
+++ ide/workspace/web/examples/angularjs/app/init/controller-registration.js
@@ -1,5 +1,5 @@
 /*global app, ControllerFactory, RegisterRoutes, RegisterData*/
-function RegisterEasyController(route, headers, controller, auto=false){
+function RegisterEasyController(route, headers, controller, auto = false) {
 	app.controller(route + 'ControllerBase', ControllerFactory(route));
 
 	app.controller(route + 'Controller', function($scope, $controller, H) {
@@ -7,43 +7,43 @@ function RegisterEasyController(route, headers, controller, auto=false){
 		$controller(route + 'ControllerBase', {
 			$scope: $scope
 		});
-		try{
+		try {
 			$controller(route + 'ControllerExtension', {
 				$scope: $scope
 			});
-		} catch (ex){
+		} catch (ex) {
 			console.log(ex);
 		}
-		
-		
-		if(auto) {
-			$scope.initTextResourcesAuto();	
-		} else{
+
+
+		if (auto) {
+			$scope.initTextResourcesAuto();
+		} else {
 			$scope.initTextResourcesEasy();
 		}
-		
-		
+
+
 		//$scope.setListHeaders(headers);
-		
+
 	});
 }
 
 //Register Easy Routes
-(function(){
-    var easyRoutes = RegisterRoutes().easyRoutes || [];
-    //var data = RegisterData();
-    
-    for (var i = 0; i < easyRoutes.length; i++) {
-        RegisterEasyController(easyRoutes[i]/*, data[easyRoutes[i]].headers*/);
-    }
+(function() {
+	var easyRoutes = RegisterRoutes().easyRoutes || [];
+	//var data = RegisterData();
+
+	for (var i = 0; i < easyRoutes.length; i++) {
+		RegisterEasyController(easyRoutes[i] /*, data[easyRoutes[i]].headers*/ );
+	}
 })();
 
 //Register Auto Routes
-(function(){
-    var autoRoutes = RegisterRoutes().autoRoutes || [];
-    //var data = RegisterData();
-    
-    for (var i = 0; i < autoRoutes.length; i++) {
-        RegisterEasyController(autoRoutes[i],null,null,true/*, data[easyRoutes[i]].headers*/);
-    }
+(function() {
+	var autoRoutes = RegisterRoutes().autoRoutes || [];
+	//var data = RegisterData();
+
+	for (var i = 0; i < autoRoutes.length; i++) {
+		RegisterEasyController(autoRoutes[i], null, null, true /*, data[easyRoutes[i]].headers*/ );
+	}
 })();
