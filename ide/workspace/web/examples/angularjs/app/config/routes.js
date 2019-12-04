3feac100469cd1bcaf25304b96521dd9be99bcf7
Om Talsania <omtalsania@geekypedia.net>
2019-12-04 18:58:38 +0530

    Update routes.js

--- ide/workspace/web/examples/angularjs/app/config/routes.js
+++ ide/workspace/web/examples/angularjs/app/config/routes.js
@@ -9,8 +9,9 @@ function RegisterRoutes() {
             {route: 'unauthorized', template: 'auth/unauthorized', controller: 'unauthorized'},
             {route: 'out-of-service', template: 'auth/out-of-service', controller: 'outOfService', auth: false},
             {route: 'settings', template: 'settings/template', controller: 'settings'}
-        ],
-        easyRoutes: ['organizations', 'users', 'groups', 'categories', 'tasks'],
-        autoRoutes: []
+        ], //customRoutes finds template inside app/modules
+        easyRoutes: ['organizations', 'users', 'groups', 'categories', 'tasks'], // Provide the names of your tables here and make a copy of app/modules/tasks with the same name
+        autoRoutes: [], // Either you can provide the names of your tables here, or you can excluded some of the routes below in autoRoutesExcludes
+        autoRoutesExcluded: [] // If you don't specify autoRoutes, all of your tables except routes defined in customRoutes and easyRoutes will automatically have UI under Masters menu. You can exclude some of those tables here
     };
 }
