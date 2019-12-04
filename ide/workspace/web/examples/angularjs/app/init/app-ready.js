ec683dd13c27dc0e1154f4fdf9350748ba13a7a4
Om Talsania <omtalsania@geekypedia.net>
2019-12-04 18:53:15 +0530

    Update app-ready.js

--- ide/workspace/web/examples/angularjs/app/init/app-ready.js
+++ ide/workspace/web/examples/angularjs/app/init/app-ready.js
@@ -2,112 +2,114 @@
 //JQuery
 SideNavStatus = false;
 
-	BuildSideNav = function(cb, mode){
-		if(!SideNavStatus){
-			BuildSideNavInternal(cb, mode);
-		}
+GLOBALS = {}
+
+BuildSideNav = function(cb, mode) {
+	if (!SideNavStatus) {
+		BuildSideNavInternal(cb, mode);
+	}
+}
+
+
+BuildSideNavInternal = function(cb, mode = "expand") {
+	$('.all-nav').show(); //CUSTOM
+
+	for (var x = navCount; x >= 0; x--) {
+		$('.all-nav').removeClass('hc-nav-' + x);
+		$('nav.hc-nav-' + x).remove();
 	}
 
+	setTimeout(function(cb) {
 
-	BuildSideNavInternal = function(cb, mode = "expand") {
-		$('.all-nav').show();//CUSTOM
-		
-		for(var x = navCount; x >= 0; x--){
-			$('.all-nav').removeClass('hc-nav-' + x);
-			$('nav.hc-nav-' + x).remove();
-		}
-		
-		setTimeout(function(cb){
-	
-			var $main_nav = $('#main-nav');
-			var $toggle = $('.toggle');
-			
-	
-			var defaultData = {
-				maxWidth: false,
-				customToggle: $toggle,
-				navTitle: '',
-				levelTitles: true,
-				pushContent: '#container',
-				insertClose: 2,
-				closeLevels: false,
-				levelOpen: mode, //overlap, expand or false,
-				levelSpacing: 0
-			};
-	
-			// add new items to original nav
-			$main_nav.find('li.add').children('a').on('click', function() {
-				var $this = $(this);
-				var $li = $this.parent();
-				var items = eval('(' + $this.attr('data-add') + ')');
-	
-				$li.before('<li class="new"><a>' + items[0] + '</a></li>');
-	
-				items.shift();
-	
-				if (!items.length) {
-					$li.remove();
-				} else {
-					$this.attr('data-add', JSON.stringify(items));
-				}
-	
-				Nav.update(true);
-			});
-	
-			// call our plugin
-			Nav = $main_nav.hcOffcanvasNav(defaultData);
-	
-			// demo settings update
-	
-			const update = (settings) => {
-				if (Nav.isOpen()) {
-					Nav.on('close.once', function() {
-						Nav.update(settings);
-						Nav.open();
-					});
-	
-					Nav.close();
-				} else {
+		var $main_nav = $('#main-nav');
+		var $toggle = $('.toggle');
+
+
+		var defaultData = {
+			maxWidth: false,
+			customToggle: $toggle,
+			navTitle: '',
+			levelTitles: true,
+			pushContent: '#container',
+			insertClose: 2,
+			closeLevels: false,
+			levelOpen: mode, //overlap, expand or false,
+			levelSpacing: 0
+		};
+
+		// add new items to original nav
+		$main_nav.find('li.add').children('a').on('click', function() {
+			var $this = $(this);
+			var $li = $this.parent();
+			var items = eval('(' + $this.attr('data-add') + ')');
+
+			$li.before('<li class="new"><a>' + items[0] + '</a></li>');
+
+			items.shift();
+
+			if (!items.length) {
+				$li.remove();
+			} else {
+				$this.attr('data-add', JSON.stringify(items));
+			}
+
+			Nav.update(true);
+		});
+
+		// call our plugin
+		Nav = $main_nav.hcOffcanvasNav(defaultData);
+
+		// demo settings update
+
+		const update = (settings) => {
+			if (Nav.isOpen()) {
+				Nav.on('close.once', function() {
 					Nav.update(settings);
-				}
-			};
-	
-			$('.actions').find('a').on('click', function(e) {
-				e.preventDefault();
-	
-				var $this = $(this).addClass('active');
-				var $siblings = $this.parent().siblings().children('a').removeClass('active');
-				var settings = eval('(' + $this.data('demo') + ')');
-	
+					Nav.open();
+				});
+
+				Nav.close();
+			} else {
+				Nav.update(settings);
+			}
+		};
+
+		$('.actions').find('a').on('click', function(e) {
+			e.preventDefault();
+
+			var $this = $(this).addClass('active');
+			var $siblings = $this.parent().siblings().children('a').removeClass('active');
+			var settings = eval('(' + $this.data('demo') + ')');
+
+			update(settings);
+		});
+
+		$('.actions').find('input').on('change', function() {
+			var $this = $(this);
+			var settings = eval('(' + $this.data('demo') + ')');
+
+			if ($this.is(':checked')) {
 				update(settings);
-			});
-	
-			$('.actions').find('input').on('change', function() {
-				var $this = $(this);
-				var settings = eval('(' + $this.data('demo') + ')');
-	
-				if ($this.is(':checked')) {
-					update(settings);
-				} else {
-					var removeData = {};
-					$.each(settings, function(index, value) {
-						removeData[index] = false;
-					});
-	
-					update(removeData);
-				}
-			});
-			
-			$('#main-nav').hide();
-			
-			SideNavStatus = true;
-			
-			if(cb) cb();
-			
-		}, 1000,  cb);
-		
-	}
-	
+			} else {
+				var removeData = {};
+				$.each(settings, function(index, value) {
+					removeData[index] = false;
+				});
+
+				update(removeData);
+			}
+		});
+
+		$('#main-nav').hide();
+
+		SideNavStatus = true;
+
+		if (cb) cb();
+
+	}, 1000, cb);
+
+}
+
 
 $(function() {
 	// $('.sidenav').sidenav({
@@ -119,9 +121,9 @@ $(function() {
 	// });	
 
 	// $('select').formSelect();
-	
-	
-	
+
+
+
 
 	function htmlbodyHeightUpdate() {
 		var height3 = $(window).height()
@@ -148,13 +150,13 @@ $(function() {
 			height2 = $('.main').height()
 			htmlbodyHeightUpdate()
 		});
-		
 
-		for(i = 100; i <= 2000; i += 100){
+
+		for (i = 100; i <= 2000; i += 100) {
 			setTimeout(function() {
 				$('.all-nav').hide();
 			}, i);
-		}	
+		}
 		// setTimeout(function() {
 		// 	$('.all-nav').hide();
 		// }, 1000);
