f0e631c0ecacad3bf7028566bc191ff4caddccd3
Om Talsania <omtalsania@geekypedia.net>
2020-01-09 19:23:40 +0530

    file upload

--- ide/workspace/web/examples/angularjs/app/components/file-model/controller.js
+++ ide/workspace/web/examples/angularjs/app/components/file-model/controller.js
@@ -1,4 +1,51 @@
 /*global app*/
+app.directive('fileUpload', function (S, upload) {
+    return {
+        restrict: 'EA',
+        scope: {
+            id: '@',
+            css: '@',
+            label: '@',
+            callback: '&',
+            url: '@'
+        },
+        template: `
+			<div class="{{containerClass}}">
+			    <input type="file" class="{{containerClass}}-input" id="{{id}}_input" name="{{id}}_input">
+			    <label class="{{containerClass}}-label" for="{{id}}_input">{{label || 'Choose a file'}}</label>
+			</div>
+        `,
+        link: function (scope, element, attr) {
+        	scope.id = scope.id || "input_" +  Math.floor(Math.random() * 1000);
+        	scope.containerClass = scope.css || 'custom-file';
+            element.bind('change', function () {
+                var formData = new FormData();
+                formData.append('file', element[0].children[0].children[0].files[0]);
+                upload(scope.url || S.baseUrl + '/files', formData, function(r){
+                	if(scope.callback) scope.callback()(r);
+                });
+            });
+
+        }
+    };
+});
+app.factory('upload', function($http) {
+  return function(url, data, callback) {
+    $http({
+      url: url,
+      method: "POST",
+      data: data,
+      headers: {
+        'Content-Type': undefined
+      }
+    }).then(function(response) {
+      if(callback) callback(response);
+    }, function(e){
+    	if(callback)  callback(e);
+    });
+  };
+});
+/*
 app.directive('fileModel', ['$parse', function ($parse) {
     return {
         restrict: 'A',
@@ -14,3 +61,4 @@ app.directive('fileModel', ['$parse', function ($parse) {
         }
     };
 }]);
+*/
