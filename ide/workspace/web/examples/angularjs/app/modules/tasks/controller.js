69b54a9c3f351078ae1406e1c99655671398dd7d
Om Talsania <omtalsania@geekypedia.net>
2020-01-16 16:27:37 +0530

    Support for async and await

--- ide/workspace/web/examples/angularjs/app/modules/tasks/controller.js
+++ ide/workspace/web/examples/angularjs/app/modules/tasks/controller.js
@@ -24,7 +24,7 @@ app.controller('tasksControllerExtension', function($scope, $controller, $rootSc
     };
 
     //This function is called when you are in list mode. i.e. after a call has returned from one of your API that returns a the paginated list of all objects matching your API.
-    $scope.onLoadAll = function(obj){
+    $scope.onLoadAll = async function(obj){
         //$scope.data.list is available here. 'obj' refers to the same. It represents the object you are trying to edit.
         
         //You can call $scope.setListHeaders(['column1','column2',...]) in case the auto generated column names are not what you wish to display.
@@ -32,31 +32,31 @@ app.controller('tasksControllerExtension', function($scope, $controller, $rootSc
     };
     
     //This function is called before the create (POST) request goes to API
-    $scope.beforeSave = function(obj, next){
+    $scope.beforeSave = async function(obj, next){
         //You can choose not to call next(), thus rejecting the save request. This can be used for extra validations.
         next();
     };
 
     //This function is called after the create (POST) request is returned from API
-    $scope.onSave = function (obj, next){
+    $scope.onSave = async function (obj, next){
         //You can choose not to call next(), thus preventing the page to display the popup that confirms the object has been created.
         next();
     };
     
     //This function is called before the update (PUT) request goes to API
-    $scope.beforeUpdate = function(obj, next){
+    $scope.beforeUpdate = async function(obj, next){
         //You can choose not to call next(), thus rejecting the update request. This can be used for extra validations.
         next();
     };
 
     //This function is called after the update (PUT) request is returned from API
-    $scope.onUpdate = function (obj, next){
+    $scope.onUpdate = async function (obj, next){
         //You can choose not to call next(), thus preventing the page to display the popup that confirms the object has been updated.
         next();
     };
     
     //This function will be called whenever there is an error during save/update operations.
-    $scope.onError = function (obj, next){
+    $scope.onError = async function (obj, next){
         //You can choose not to call next(), thus preventing the page to display the popup that confirms there has been an error.
         next();
     };
