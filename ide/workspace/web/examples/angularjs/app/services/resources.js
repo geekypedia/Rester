484a77a22d64dbd043a3ee4bdc9a15a8cf2b8d1b
Om Talsania <omtalsania@geekypedia.net>
2020-01-16 15:30:17 +0530

    Update resources.js

--- ide/workspace/web/examples/angularjs/app/services/resources.js
+++ ide/workspace/web/examples/angularjs/app/services/resources.js
@@ -7,7 +7,7 @@ app.service('R', function($resource, $http, S) {
 				id: '@id'
 			});
 		},
-		count: function(resourceName, cb) {
+		count: async function(resourceName, cb) {
 			$http.get(S.baseUrl + '/' + resourceName + '/?count=true')
 				.then(function(results) {
 					if (results && results.data && results.data.length > 0)
