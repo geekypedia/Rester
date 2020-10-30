f9048afb2f118f59cd8c5e3e12d4b26d1b168bb4
Om Talsania <omtalsania@geekypedia.net>
2020-10-30 18:11:03 +0530

    Update controller.js

--- ide/workspace/web/examples/angularjs/app/components/nav/controller.js
+++ ide/workspace/web/examples/angularjs/app/components/nav/controller.js
@@ -14,16 +14,18 @@ app.controller('navController', async function($scope, $http, S, H) {
     	data.push(masterMenu);
     }
     
+    var prefix = '#!';
+    
     
     for(var k in data){
         if(data.hasOwnProperty(k) && data[k].items && data[k].items.length > 0){
             for (var i = 0; i < data[k].items.length; i++) {
-                if(!data[k].items[i].action.startsWith('#!')) data[k].items[i].action =  '#!' + data[k].items[i].action;
+                if(!data[k].items[i].action.startsWith(prefix)) data[k].items[i].action =  prefix + data[k].items[i].action;
                 if(data[k].items[i].color && !data[k].items[i].color.startsWith("col-")) data[k].items[i].color = "col-" + data[k].items[i].color;
                 if(data[k].items[i].items && data[k].items[i].items.length > 0){
                     data[k].items[i].action = '';
                     for (var j = 0; j < data[k].items[i].items.length; j++) {
-                        if(!data[k].items[i].items[j].action.startsWith('#!')) data[k].items[i].items[j].action = '#!' + data[k].items[i].items[j].action;
+                        if(!data[k].items[i].items[j].action.startsWith(prefix)) data[k].items[i].items[j].action = prefix + data[k].items[i].items[j].action;
                         if(data[k].items[i].items[j].color  && !data[k].items[i].items[j].color.startsWith("col-")) data[k].items[i].items[j].color = "col-" + data[k].items[i].items[j].color;
                     }
                 }
@@ -35,12 +37,12 @@ app.controller('navController', async function($scope, $http, S, H) {
 	    for(var k in data){
 	        if(data.hasOwnProperty(k) && data[k].items && data[k].items.length > 0){
 	            for (var i = 0; i < data[k].items.length; i++) {
-	                if(!data[k].items[i].action.startsWith('#!')) data[k].items[i].action =  '#!' + data[k].items[i].action;
+	                if(!data[k].items[i].action.startsWith(prefix)) data[k].items[i].action =  prefix + data[k].items[i].action;
 	                if(data[k].items[i].color && !data[k].items[i].color.startsWith("col-")) data[k].items[i].color = "col-" + data[k].items[i].color;
 	                if(data[k].items[i].items && data[k].items[i].items.length > 0){
 	                    data[k].items[i].action = '';
 	                    for (var j = 0; j < data[k].items[i].items.length; j++) {
-	                        if(!data[k].items[i].items[j].action.startsWith('#!')) data[k].items[i].items[j].action = '#!' + data[k].items[i].items[j].action;
+	                        if(!data[k].items[i].items[j].action.startsWith(prefix)) data[k].items[i].items[j].action = prefix + data[k].items[i].items[j].action;
 	                        if(data[k].items[i].items[j].color  && !data[k].items[i].items[j].color.startsWith("col-")) data[k].items[i].items[j].color = "col-" + data[k].items[i].items[j].color;
 	                    }
 	                }
